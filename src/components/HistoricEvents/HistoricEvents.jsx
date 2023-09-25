b98c005387c0764cd1b58dc78aa18d8493881b37
ASGJxy <armasjack11@gmail.com>
2023-09-24 20:14:47 -0500

    feat: scaffolding

--- src/components/HistoricEvents/HistoricEvents.jsx
+++ src/components/HistoricEvents/HistoricEvents.jsx
@@ -3,11 +3,13 @@ import styles from "./historicevents.module.css";
 export const HistoricEvents = () => {
   return (
     <section className={styles.fifthB}>
-      <div className={styles.infoContainer}>
-        Quién liberó a <br />
-        Hugo Chávez?
+      <div className={styles.textContainer}>
+        <div className={styles.infoContainer}>
+          Quién liberó a <br />
+          Hugo Chávez?
+        </div>
+        <div className={styles.clickContainer}>Clickea para descubrirlo</div>
       </div>
-      <div className={styles.clickContainer}>Clickea para descubrirlo</div>
     </section>
   );
 };
--- src/components/QuestionsBlock/QuestionsBlock.jsx
+++ src/components/QuestionsBlock/QuestionsBlock.jsx
@@ -10,7 +10,21 @@ export const QuestionsBlock = () => {
       <h2 className={styles.noteQuestion}>
         ¡Envianosla y la tomaremos en cuenta!
       </h2>
-      <input className={styles.identityQuestion} />
+      <input
+        className={styles.identityQuestion}
+        type="text"
+        placeholder="Nombre y apellido"
+      />
+
+      <input className={styles.anonymousBox} type="checkbox" />
+      <h3 className={styles.formQuestion}>
+        ¿Qué información no puede faltar en nuestra biblioteca digital?
+      </h3>
+      <input
+        className={styles.visitorQuestion}
+        type="text"
+        placeholder="Mi pregunta es..."
+      />
     </section>
   );
 };
